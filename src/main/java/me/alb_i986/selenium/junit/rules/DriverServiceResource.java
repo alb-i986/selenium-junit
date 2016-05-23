96ff4431d95ecda371e7378c5c906b5f84ebed56
Alberto Scotto <alberto.scotto@paddypower.com>
2016-05-23 17:23:30 +0100

    fixup! DriverServiceResource

--- src/main/java/me/alb_i986/selenium/junit/rules/DriverServiceResource.java
+++ src/main/java/me/alb_i986/selenium/junit/rules/DriverServiceResource.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.remote.service.DriverService;
 /**
  * A {@link org.junit.rules.TestRule} managing {@link DriverService} instances.
  * <p>
- * The service is started on test start, and stopped on test termination.
+ * The {@link DriverService} is started on test start, and stopped on test termination.
  * <p>
- * This rule should be used as a static {@link org.junit.ClassRule}.
+ * This rule is typically used as a {@link org.junit.ClassRule}.
  */
 public class DriverServiceResource extends ExternalResource {
 
