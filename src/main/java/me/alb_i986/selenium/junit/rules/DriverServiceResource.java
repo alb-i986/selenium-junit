4690fb773fc2f63628cd1a4878e5d1d07ef0914d
Alberto Scotto <alberto.scotto@paddypower.com>
2016-05-23 12:21:53 +0100

    DriverServiceResource
    - improve javadoc
    - improve unit tests

--- src/main/java/me/alb_i986/selenium/junit/rules/DriverServiceResource.java
+++ src/main/java/me/alb_i986/selenium/junit/rules/DriverServiceResource.java
@@ -3,19 +3,26 @@ package me.alb_i986.selenium.junit.rules;
 import org.junit.rules.ExternalResource;
 import org.openqa.selenium.remote.service.DriverService;
 
+/**
+ * A {@link org.junit.rules.TestRule} managing {@link DriverService} instances.
+ * <p>
+ * The service is started on test start, and stopped on test termination.
+ * <p>
+ * This rule should be used as a static {@link org.junit.ClassRule}.
+ */
 public class DriverServiceResource extends ExternalResource {
 
     private final DriverService service;
 
     public DriverServiceResource(DriverService service) {
         if (service == null) {
-            throw new IllegalArgumentException("service should not be null");
+            throw new IllegalArgumentException("The DriverService should not be null");
         }
         this.service = service;
     }
 
     /**
-     * Start the service
+     * Start the service.
      *
      * @see DriverService#start()
      */
@@ -25,7 +32,7 @@ public class DriverServiceResource extends ExternalResource {
     }
 
     /**
-     * Stop the service
+     * Stop the service.
      *
      * @see DriverService#stop()
      */
--- src/test/java/me/alb_i986/selenium/junit/rules/DriverServiceResourceTest.java
+++ src/test/java/me/alb_i986/selenium/junit/rules/DriverServiceResourceTest.java
@@ -6,13 +6,26 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openqa.selenium.remote.service.DriverService;
 
-import static org.junit.Assert.*;
-import static org.mockito.BDDMockito.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DriverServiceResourceTest {
 
     @Mock private DriverService mockedService;
+
+    @Test
+    public void nullDriverServiceShouldNotBeAllowed() {
+        try {
+            new DriverServiceResource(null);
+            fail("null DriverService should not be allowed");
+        } catch (IllegalArgumentException e) {
+            // expected
+            assertThat(e.getMessage(), equalTo("The DriverService should not be null"));
+        }
+    }
 
     @Test
     public void beforeShouldStartTheService() throws Throwable {
@@ -32,15 +45,5 @@ public class DriverServiceResourceTest {
         sut.after();
 
         verify(mockedService).stop();
-    }
-
-    @Test
-    public void nullArgShouldNotBeAllowed() {
-        try {
-            new DriverServiceResource(null);
-            fail("null DriverService should not be allowed");
-        } catch (IllegalArgumentException e) {
-            // expected
-        }
     }
 }
